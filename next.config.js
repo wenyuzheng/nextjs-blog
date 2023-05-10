fd6e29d8e34989c4b1d8193b744c84c6cda36a66
Wenyu Zheng <zhengwy229@gmail.com>
2023-05-10 23:37:23 +0100

    Template created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react/jsx-runtime.js": require.resolve("react/jsx-runtime"),
+    };
+
+    config.resolve = {
+      ...config.resolve,
+
+      fallback: {
+        ...config.resolve.fallback,
+        child_process: false,
+        fs: false,
+        // 'builtin-modules': false,
+        // worker_threads: false,
+      },
+    };
+
+    return config;
+  },
+};
